0d2cce106dbfe94acf8d96979698cc4cf0c90fba
Maksim Varabei <maxvorobei15@gmail.com>
2025-03-31 00:24:13 +0200

    init: add solutions for 1-module

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  if (!str) {return '';}
+  return str = str[0].toUpperCase() + str.slice(1);
 }
+
+// console.log(ucFirst(''));
+
+// console.log(ucFirst('ksd'));
