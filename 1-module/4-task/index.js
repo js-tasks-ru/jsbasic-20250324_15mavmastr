015727515327e1964eaaf7c50ca87267ca30c01f
Maksim Varabei <maxvorobei15@gmail.com>
2025-04-01 14:57:04 +0200

    fix: correct a defect

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,4 @@
 function checkSpam(str) {
   // if (str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx')) {return true;}
-  return Boolean(str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx'));
+  return str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx');
 }
