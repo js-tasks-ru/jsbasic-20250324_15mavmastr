daf8e72d3d70a0fed6944f276e15d23d78f6cbbc
Maksim Varabei <maxvorobei15@gmail.com>
2025-04-01 14:50:06 +0200

    fix: added corrections based on comments

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,9 +10,9 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (typeof name !== 'string') {return false;}
-  if (name.length >= 4 && !name.includes(' ')) {return true;}
-  return false;
+  // if (typeof name !== 'string') {return false;}
+  // if (name.length >= 4 && !name.includes(' ')) {return true;}
+  return Boolean(name) && name.length >= 4 && !name.includes(' ');
 }
 
 function sayHello() {
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,4 @@
 function checkSpam(str) {
-  if (str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx')) {return true;}
-  return false;
+  // if (str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx')) {return true;}
+  return Boolean(str.toLowerCase().includes('1xbet') || str.toLowerCase().includes('xxx'));
 }
