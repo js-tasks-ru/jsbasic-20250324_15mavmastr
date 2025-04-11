f9d61d73e9f96b01f6eac6926bb1c8b418d53f97
Maksim Varabei <maxvorobei15@gmail.com>
2025-04-12 00:24:50 +0200

    init: add solutions for 4-module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.tBodies[0].rows) {
+    const ageCell = row.cells[1];
+    const genderCell = row.cells[2];
+    const statusCell = row.cells[3];
+
+    if (statusCell.hasAttribute('data-available')) {
+      if (statusCell.dataset.available === 'true') {
+        row.classList.add('available');
+      } else {
+        row.classList.add('unavailable');
+      }
+    } else {row.hidden = true;}
+
+    if (genderCell.textContent === 'm') {
+      row.classList.add('male');
+    } else if (genderCell.textContent === 'f') {
+      row.classList.add('female');
+    }
+
+    if (parseInt(ageCell.textContent, 10) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
